3a3a1060e85a3293cfe66ae9eb691460934f71a8
Jose Bocanegra <j.bocanegra@uniandes.edu.co>
2020-05-08 14:04:11 -0500

    Added sql file

---
+++ sql/data.sql
@@ -0,0 +1,20 @@
+INSERT INTO public.album(
+	id, name, cover, "releaseDate", description, genre, "recordLabel")
+	VALUES (100,'Buscando América', 'https://i.pinimg.com/564x/aa/5f/ed/aa5fed7fac61cc8f41d1e79db917a7cd.jpg', 'Buscando América es el primer álbum de la banda de Rubén Blades y Seis del Solar lanzado en 1984. La producción, bajo el sello Elektra, fusiona diferentes ritmos musicales tales como la salsa, reggae, rock, y el jazz latino. El disco fue grabado en Eurosound Studios en Nueva York entre mayo y agosto de 1983.', '1984-08-01','Salsa', 'Elektra');
+
+INSERT INTO public.album(
+	id, name, cover, "releaseDate", description, genre, "recordLabel")
+	VALUES (101,'Buscando América', 'https://cdn.shopify.com/s/files/1/0275/3095/products/image_4931268b-7acf-4702-9c55-b2b3a03ed999_1024x1024.jpg', 'Buscando América es el primer álbum de la banda de Rubén Blades y Seis del Solar lanzado en 1984. La producción, bajo el sello Elektra, fusiona diferentes ritmos musicales tales como la salsa, reggae, rock, y el jazz latino. El disco fue grabado en Eurosound Studios en Nueva York entre mayo y agosto de 1983.', '1984-08-01', 'Salsa', 'Elektra');
+
+    INSERT INTO public.album(
+	id, name, cover, "releaseDate", description, genre, "recordLabel")
+	VALUES (102,'A Night at the Opera', 'https://upload.wikimedia.org/wikipedia/en/4/4d/Queen_A_Night_At_The_Opera.png', 'Es el cuarto álbum de estudio de la banda británica de rock Queen, publicado originalmente en 1975. Coproducido por Roy Thomas Baker y Queen, A Night at the Opera fue, en el tiempo de su lanzamiento, la producción más cara realizada.1​ Un éxito comercial, el álbum fue votado por el público y citado por publicaciones musicales como uno de los mejores trabajos de Queen y de la historia del rock.', '1975-11-21', 'Rock', 'EMI');
+
+    INSERT INTO public.album(
+	id, name, cover, "releaseDate", description, genre, "recordLabel")
+	VALUES (103,'A Day at the Races', 'https://www.udiscovermusic.com/wp-content/uploads/2019/11/a-day-at-the-races.jpg', 'El álbum fue grabado en los Estudios Sarm West, The Manor and Wessex en Inglaterra y con el ingeniero Mike Stone. El título del álbum es una referencia directa al anterior, A Night at the Opera. Ambos álbumes están titulados como películas de los hermanos Marx.', '1976-12-10', 'Rock', 'EMI');
+
+     INSERT INTO public.album(
+	id, name, cover, "releaseDate", description, genre, "recordLabel")
+	VALUES (104,'Cantora', 'https://www.cancioneros.com/imatges/1987.jpg', 'Cantora es un álbum doble de duetos de la cantante argentina Mercedes Sosa con diversos intérpretes  y el último proyecto musical grabado por ella. Aunque la cantante ya había sufrido diversos problemas de salud cuando se grabó, el disco fue un éxito inmediato y se convirtió en disco de oro en Argentina en un tiempo récord. El primer disco logró obtener dos Grammy Latinos 2009, por Mejor álbum folclórico y Mejor diseño de empaque', '2009-03-27', 'Folk', 'Sony Music');
+
