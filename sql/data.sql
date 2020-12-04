9d63be12fadbc02a91317cdf6f9d25524c77ac07
Jose Bocanegra <j.bocanegra@uniandes.edu.co>
2020-12-04 14:40:40 -0500

    Update sql file

--- sql/data.sql
+++ sql/data.sql
@@ -1,5 +1,13 @@
-delete from public.album;
+﻿delete from public.performer_albums_album;
+delete from public.collector_album;
+delete from public.performer_collectors_collector;
+delete from public.performer_prize;
 delete from public.performer;
+delete from public.comment;
+delete from track;
+delete from public.album;
+delete from public.collector;
+delete from public.prize;
 
 INSERT INTO public.album(
 	id, name, cover, "releaseDate", description, genre, "recordLabel")
@@ -7,7 +15,7 @@ INSERT INTO public.album(
 
 INSERT INTO public.album(
 	id, name, cover, "releaseDate", description, genre, "recordLabel")
-	VALUES (101,'Poeta del pueblo', 'https://cdn.shopify.com/s/files/1/0275/3095/products/image_4931268b-7acf-4702-9c55-b2b3a03ed999_1024x1024.jpg', '1984-08-01', 'Buscando América es el primer álbum de la banda de Rubén Blades y Seis del Solar lanzado en 1984. La producción, bajo el sello Elektra, fusiona diferentes ritmos musicales tales como la salsa, reggae, rock, y el jazz latino. El disco fue grabado en Eurosound Studios en Nueva York entre mayo y agosto de 1983.', 'Salsa', 'Elektra');
+	VALUES (101,'Poeta del pueblo', 'https://cdn.shopify.com/s/files/1/0275/3095/products/image_4931268b-7acf-4702-9c55-b2b3a03ed999_1024x1024.jpg', '1984-08-01', 'Recopilación de 27 composiciones del cosmos Blades que los bailadores y melómanos han hecho suyas en estos 40 años de presencia de los ritmos y concordias afrocaribeños en múltiples escenarios internacionales. Grabaciones de Blades para la Fania con las orquestas de Pete Rodríguez, Ray Barreto, Fania All Stars y, sobre todo, los grandes éxitos con la Banda de Willie Colón', 'Salsa', 'Elektra');
 
 INSERT INTO public.album(
 	id, name, cover, "releaseDate", description, genre, "recordLabel")
@@ -17,30 +25,111 @@ INSERT INTO public.album(
 	id, name, cover, "releaseDate", description, genre, "recordLabel")
 	VALUES (103,'A Day at the Races', 'https://www.udiscovermusic.com/wp-content/uploads/2019/11/a-day-at-the-races.jpg', '1976-12-10', 'El álbum fue grabado en los Estudios Sarm West, The Manor and Wessex en Inglaterra y con el ingeniero Mike Stone. El título del álbum es una referencia directa al anterior, A Night at the Opera. Ambos álbumes están titulados como películas de los hermanos Marx.', 'Rock', 'EMI');
 
-INSERT INTO public.album(
-	id, name, cover, "releaseDate", description, genre, "recordLabel")
-	VALUES (104,'Cantora', 'https://www.cancioneros.com/imatges/1987.jpg','2009-03-27', 'Cantora es un álbum doble de duetos de la cantante argentina Mercedes Sosa con diversos intérpretes  y el último proyecto musical grabado por ella. Aunque la cantante ya había sufrido diversos problemas de salud cuando se grabó, el disco fue un éxito inmediato y se convirtió en disco de oro en Argentina en un tiempo récord. El primer disco logró obtener dos Grammy Latinos 2009, por Mejor álbum folclórico y Mejor diseño de empaque', 'Folk', 'Sony Music');
-
---Inserting bands
-
-INSERT INTO public.performer(
-	id, name, image, description, "birthDate", "creationDate", type, "bandId")
-	VALUES (100, 'Queen', 'https://pm1.narvii.com/6724/a8b29909071e9d08517b40c748b6689649372852v2_hq.jpg', 'Queen es una banda británica de rock formada en 1970 en Londres por el cantante Freddie Mercury, el guitarrista Brian May, el baterista Roger Taylor y el bajista John Deacon. Si bien el grupo ha presentado bajas de dos de sus miembros (Mercury, fallecido en 1991, y Deacon, retirado en 1997), los integrantes restantes, May y Taylor, continúan trabajando bajo el nombre Queen, por lo que la banda aún se considera activa.',
-	null, '1970-01-01', 'Band' , null);
-
-INSERT INTO public.performer(
-	id, name, image, description, "birthDate", "creationDate", type, "bandId")
-	VALUES (101, 'Soda Stereo', 'https://upload.wikimedia.org/wikipedia/commons/6/6f/CeratiAlbertiBosio.jpg', 'Soda Stereo fue una banda argentina de rock formada en Buenos Aires en 1982 por Gustavo Cerati (voz, guitarra), Héctor «Zeta» Bosio (bajo) y Carlos Alberto Ficicchia «Charly Alberti» (batería), considerada una de las más influyentes e importantes bandas iberoamericanas de todos los tiempos y una leyenda de la música latina. Fueron el primer grupo de habla hispana en conseguir un éxito masivo en Latinoamérica y tuvieron un papel muy importante en el desarrollo y la difusión del rock iberoamericano y el rock en español durante las décadas de 1980 y 1990. Durante su carrera, fueron vanguardistas y marcaron tendencia en Latinoamérica, siendo protagonistas en diversos géneros como la música divertida de sus inicios, la new wave, el dark, el hard rock, el rock alternativo y el rock electrónico de sus finales.',
-	null, '1982-01-01', 'Band' , null);
-
 --Inserting musicians
 
 INSERT INTO public.performer(
 	id, name, image, description, "birthDate", "creationDate", type, "bandId")
-	VALUES (102, 'Rubén Blades Bellido de Luna', 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Ruben_Blades_by_Gage_Skidmore.jpg/800px-Ruben_Blades_by_Gage_Skidmore.jpg', 'Es un cantante, compositor, músico, actor, abogado, político y activista panameño. Ha desarrollado gran parte de su carrera artística en la ciudad de Nueva York.',
+	VALUES (100, 'Rubén Blades Bellido de Luna', 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Ruben_Blades_by_Gage_Skidmore.jpg/800px-Ruben_Blades_by_Gage_Skidmore.jpg', 'Es un cantante, compositor, músico, actor, abogado, político y activista panameño. Ha desarrollado gran parte de su carrera artística en la ciudad de Nueva York.',
 	'1948-07-16', null, 'Musician' , null);
+	
+--Inserting bands
 
 INSERT INTO public.performer(
 	id, name, image, description, "birthDate", "creationDate", type, "bandId")
-	VALUES (103, 'Joan Manuel Serrat Teresa', 'https://upload.wikimedia.org/wikipedia/commons/e/e3/Serrat.jpg', 'Es un cantautor, compositor, actor, escritor, poeta y músico español.',
-	'1943-12-27', null, 'Musician' , null);
+	VALUES (101, 'Queen', 'https://pm1.narvii.com/6724/a8b29909071e9d08517b40c748b6689649372852v2_hq.jpg', 'Queen es una banda británica de rock formada en 1970 en Londres por el cantante Freddie Mercury, el guitarrista Brian May, el baterista Roger Taylor y el bajista John Deacon. Si bien el grupo ha presentado bajas de dos de sus miembros (Mercury, fallecido en 1991, y Deacon, retirado en 1997), los integrantes restantes, May y Taylor, continúan trabajando bajo el nombre Queen, por lo que la banda aún se considera activa.',
+	null, '1970-01-01', 'Band' , null);
+
+--Inserting musician - album
+
+INSERT INTO public.performer_albums_album(
+	"performerId", "albumId")
+	VALUES(100, 100);
+
+INSERT INTO public.performer_albums_album(
+	"performerId", "albumId")
+	VALUES(100, 101);
+
+--Inserting band - album
+
+INSERT INTO public.performer_albums_album(
+	"performerId", "albumId")
+	VALUES(101, 102);
+
+INSERT INTO public.performer_albums_album(
+	"performerId", "albumId")
+	VALUES(101, 103);
+
+--Inserting collector
+
+INSERT INTO public.collector(
+	id, name, telephone, email)
+	VALUES(100, 'Manolo Bellon', '3502457896', 'manollo@caracol.com.co');
+
+INSERT INTO public.collector(
+	id, name, telephone, email)
+	VALUES(101, 'Jaime Monsalve', '3012357936', 'jmonsalve@rtvc.com.co');
+
+--Inserting collector_album
+
+INSERT INTO public.collector_album(
+	id, price, status, "albumId", "collectorId")
+	VALUES(100, 35, 'Active', 100, 100);
+
+INSERT INTO public.collector_album(
+	id, price, status, "albumId", "collectorId")
+	VALUES(101, 25, 'Active', 101, 101);
+
+--Inserting comment
+
+INSERT INTO public.comment(
+	id, description, rating, "albumId", "collectorId")
+	VALUES(100, 'The most relevant album of Ruben Blades', 5, 100, 100);
+
+INSERT INTO public.comment(
+	id, description, rating, "albumId", "collectorId")
+	VALUES(101, 'I love this album of Queen', 5, 102, 101);
+
+--Inserting performer - collector
+
+INSERT INTO public.performer_collectors_collector(
+	"performerId", "collectorId")
+	VALUES(100, 100);
+
+INSERT INTO public.performer_collectors_collector(
+	"performerId", "collectorId")
+	VALUES(101, 101);
+
+
+--Inserting prize
+
+INSERT INTO public.prize(
+	id, organization, name, description)
+	VALUES(100, 'National Academy of Recording Arts & Sciences', 'Grammy Award', 'Grammy Award, any of a series of awards presented annually in the United States by the National Academy of Recording Arts & Sciences');
+
+INSERT INTO public.prize(
+	id, organization, name, description)
+	VALUES(101, 'Univisión', 'Premios lo Nuestro', 'Es una ceremonia de entrega de premios a los mejores músicos latinos del año. Premio Lo Nuestro es presentado por la cadena de televisión Univisión, y fueron creados en 1989, siendo la entrega de premios más antigua en la historia musical latina.');
+
+
+--Inserting performer prize
+
+INSERT INTO public.performer_prize(
+	id, "premiationDate", "prizeId", "performerId")
+	VALUES(100, '1978-12-10', 100, 100);
+
+INSERT INTO public.performer_prize(
+	id, "premiationDate", "prizeId", "performerId")
+	VALUES(101, '1980-12-10', 101, 101);
+
+
+--Inserting track
+
+INSERT INTO public.track(
+	id, name, duration, "albumId")
+	VALUES(100, 'Decisiones', '5:05', 100);
+
+INSERT INTO public.track(
+	id, name, duration, "albumId")
+	VALUES(101, 'Desapariciones', '6:29', 100);
+	
+
