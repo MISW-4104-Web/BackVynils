9e9fad2ce9ff71a73236bbcbed2e16a213e7e4d8
Jose Bocanegra <j.bocanegra@uniandes.edu.co>
2021-03-25 11:48:47 -0500

    Add tests for album

--- src/album/album.controller.spec.ts
+++ src/album/album.controller.spec.ts
@@ -1,18 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AlbumController } from './album.controller';
+import { AlbumService } from './album.service';
 
 describe('Album Controller', () => {
-  let controller: AlbumController;
+  let controller: AlbumController
+  let service: AlbumService
+
+  const mockAlbumService = {
+    findAll: () => [{
+      id: 1,
+      name: "x" ,
+      cover: "x",
+      releaseDate: new Date(), 
+      description: "x",
+      genre: "x",
+      recordLabel: "x" 
+    }],
+  };
+
+  const albumServiceProvider = {
+    provide: AlbumService,
+    useValue: mockAlbumService,
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AlbumController],
+      providers: [albumServiceProvider]
     }).compile();
 
     controller = module.get<AlbumController>(AlbumController);
+    service = module.get<AlbumService>(AlbumService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('findAll', async () => {
+    expect(await controller.findAll()).toEqual(mockAlbumService.findAll());
   });
 });
--- src/album/album.service.spec.ts
+++ src/album/album.service.spec.ts
@@ -1,15 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Album } from './album.entity';
 import { AlbumService } from './album.service';
+
+export class AlbumRepositoryFake {
+  public create(): void {}
+  public async save(): Promise<void> {}
+  public async remove(): Promise<void> {}
+  public async findOne(): Promise<void> {}
+}
 
 describe('AlbumService', () => {
   let service: AlbumService;
+  let repository: Repository<AlbumService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AlbumService],
+      providers: [AlbumService, {
+        provide: getRepositoryToken(Album),
+        useClass: AlbumRepositoryFake,
+      },],
     }).compile();
 
     service = module.get<AlbumService>(AlbumService);
+    repository = module.get(getRepositoryToken(Album));
   });
 
   it('should be defined', () => {
