df27f8f831848f93d5016cbed890402b583784c7
Jose Bocanegra <j.bocanegra@uniandes.edu.co>
2021-03-26 12:30:01 -0500

    Add test for Album

--- src/album/album.service.spec.ts
+++ src/album/album.service.spec.ts
@@ -1,33 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { AlbumDTO } from './album.dto';
 import { Album } from './album.entity';
 import { AlbumService } from './album.service';
 
-export class AlbumRepositoryFake {
-  public create(): void {}
-  public async save(): Promise<void> {}
-  public async remove(): Promise<void> {}
-  public async findOne(): Promise<void> {}
-}
-
 describe('AlbumService', () => {
   let service: AlbumService;
-  let repository: Repository<AlbumService>;
+  let repositoryMock: MockType<Repository<Album>>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AlbumService, {
-        provide: getRepositoryToken(Album),
-        useClass: AlbumRepositoryFake,
-      },],
+      providers: [
+        AlbumService, {
+          provide: getRepositoryToken(Album),
+          useFactory: repositoryMockFactory
+        },
+      ],
     }).compile();
 
     service = module.get<AlbumService>(AlbumService);
-    repository = module.get(getRepositoryToken(Album));
+    repositoryMock = module.get(getRepositoryToken(Album));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 });
+
+// @ts-ignore
+export const repositoryMockFactory: () => MockType<Repository<any>> = jest.fn(() => ({
+  findOne: jest.fn(),
+  find: jest.fn(),
+  update: jest.fn(),
+  save: jest.fn()
+}));
+
+export type MockType<T> = {
+  [P in keyof T]: jest.Mock<{}>;
+};
