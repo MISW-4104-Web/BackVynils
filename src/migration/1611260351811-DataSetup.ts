5e282065c5547c0e63216b6b7b1d5b250b7b1655
Santiago Liñán <santiago.linan@frubana.com>
2021-04-09 18:04:40 -0500

    New migration

--- src/migration/1611260351811-DataSetup.ts
+++ src/migration/1611260351811-DataSetup.ts
@@ -3,8 +3,10 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 export class DataSetup1611260351811 implements MigrationInterface {
 
     migration = `
+
         INSERT INTO public.album(id, name, cover, "releaseDate", description, genre, "recordLabel")
-        VALUES (100, 'Buscando América', 'https://i.pinimg.com/564x/aa/5f/ed/aa5fed7fac61cc8f41d1e79db917a7cd.jpg', '1984-08-01',
+        VALUES (100, 'Buscando América', 'https://i.pinimg.com/564x/aa/5f/ed/aa5fed7fac61cc8f41d1e79db917a7cd.jpg',
+                '1984-08-01',
                 'Buscando América es el primer álbum de la banda de Rubén Blades y Seis del Solar lanzado en 1984. La producción, bajo el sello Elektra, fusiona diferentes ritmos musicales tales como la salsa, reggae, rock, y el jazz latino. El disco fue grabado en Eurosound Studios en Nueva York entre mayo y agosto de 1983.',
                 'Salsa', 'Elektra');
 
@@ -16,12 +18,14 @@ export class DataSetup1611260351811 implements MigrationInterface {
                 'Salsa', 'Elektra');
 
         INSERT INTO public.album(id, name, cover, "releaseDate", description, genre, "recordLabel")
-        VALUES (102, 'A Night at the Opera', 'https://upload.wikimedia.org/wikipedia/en/4/4d/Queen_A_Night_At_The_Opera.png', '1975-11-21',
+        VALUES (102, 'A Night at the Opera',
+                'https://upload.wikimedia.org/wikipedia/en/4/4d/Queen_A_Night_At_The_Opera.png', '1975-11-21',
                 'Es el cuarto álbum de estudio de la banda británica de rock Queen, publicado originalmente en 1975. Coproducido por Roy Thomas Baker y Queen, A Night at the Opera fue, en el tiempo de su lanzamiento, la producción más cara realizada.1​ Un éxito comercial, el álbum fue votado por el público y citado por publicaciones musicales como uno de los mejores trabajos de Queen y de la historia del rock.',
                 'Rock', 'EMI');
 
         INSERT INTO public.album(id, name, cover, "releaseDate", description, genre, "recordLabel")
-        VALUES (103, 'A Day at the Races', 'https://www.udiscovermusic.com/wp-content/uploads/2019/11/a-day-at-the-races.jpg', '1976-12-10',
+        VALUES (103, 'A Day at the Races',
+                'https://www.udiscovermusic.com/wp-content/uploads/2019/11/a-day-at-the-races.jpg', '1976-12-10',
                 'El álbum fue grabado en los Estudios Sarm West, The Manor and Wessex en Inglaterra y con el ingeniero Mike Stone. El título del álbum es una referencia directa al anterior, A Night at the Opera. Ambos álbumes están titulados como películas de los hermanos Marx.',
                 'Rock', 'EMI');
 
